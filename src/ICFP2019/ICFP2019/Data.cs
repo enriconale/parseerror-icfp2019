a0d3af1e1e76dd9997d33df72b992070fff9d102
Alvise Spanò <spano.alvise@gmail.com>
2019-06-22 13:01:30 +0200

    Update Data.cs

--- src/ICFP2019/ICFP2019/Data.cs
+++ src/ICFP2019/ICFP2019/Data.cs
@@ -1,6 +1,6 @@
 ﻿
-
 using System;
+
 
 namespace ICFP2019
 {
@@ -10,27 +10,47 @@ namespace ICFP2019
         Empty = 0, Filled = 1, Obstacle = 2
     }
 
-    class Map
+    class Map<T> where T : struct
     {
-        private Tile[,] a;
+        private T[,] a;
         private int w, h;
 
         public Map(int w, int h)
         {
-            this.w = w;
-            this.h = h;
-            a = new Tile[w, h];
+            this.W = w;
+            this.H = h;
+            a = new T[w, h];
             Array.Clear(a, 0, w * h);
         }
 
+        public T this[int x, int y]
+        {
+            get
+            {
+                return a[x, y];
+            }
+
+            set
+            {
+                a[x, y] = value;
+            }
+        }
+
+        public int W { get => w; set => w = value; }
+        public int H { get => h; set => h = value; }
     }
+
 
     class Status
     {
-        private Map map;
+        private Map<Tile> map;
 
-        public Status()
+        public Status(Map<Tile> map)
         {
-
+            this.map = map;
         }
+
+        public Map<Tile> Map { get => map; set => map = value; }
+    }
+
 }
