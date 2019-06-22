7ac48c34d7139b9ade8ca971f89a13cbba0f7b56
giop <mgaion@gmail.com>
2019-06-22 14:09:17 +0200

    Added problems and Parser.cs

---
+++ src/ICFP2019/ICFP2019/Parser.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICFP2019
+{
+    class Parser
+    {
+        private static List<Line> verticalLines = new List<Line>();
+        private static List<Line> horizontalLines = new List<Line>();
+
+        public static Status parseProblem(String problem) {
+
+            throw new Exception();
+        }
+
+        public static Map<Tile> parseMap(String map, List<String> obstacles) {
+
+
+
+            throw new Exception();
+        }
+
+
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(String coords) {
+            var cs = coords.Split(',');
+            if (cs.Length != 2) throw new Exception("Point coords parsing error: " + coords);
+            try
+            {
+                this.x = int.Parse(cs[0]);
+                this.y = int.Parse(cs[1]);
+                //if float add 0.5
+            }
+            catch (Exception e) {
+                //e.Message = "Point coords parsing error: " + coords + " \n" + e.Message;
+                throw e;                
+            }
+        }
+
+        public bool isCollinear(Point other)
+        {
+            return x == other.x || y == other.y;
+        }
+    }
+
+    class Vertex : Point
+    {
+        public Vertex(string coords) : base(coords)
+        {
+        }
+    }
+
+    class Line {
+        public Vertex s;
+        public Vertex e;
+
+        public Line(Vertex s, Vertex e) {
+            if (!s.isCollinear(e)) throw new Exception("Invalid Line Endpoints");
+            this.s = s;
+            this.e = e;
+        }
+
+        public bool intersect(Point p)
+        {
+            return isBetween(s.x, e.x, p.x) || isBetween(s.y, e.y, p.y);
+        }
+
+        public bool isBelow(Point p)
+        {
+            return (s.x == e.x && p.y <= s.y) || (s.y == e.y && p.x <= s.x);
+        }
+
+        public bool isBetween(float a, float b, float v) {
+            return (a <= v && v < b) || (a > v && v >= b);
+        }
+
+        public bool isBetween(int a, int b, int v)
+        {
+            return (a <= v && v < b) || (a > v && v >= b);
+        }
+
+    }
+
+}
