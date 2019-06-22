481e57117e457f09d43b40eaf07c5adee0381185
Alvise Spanò <spano.alvise@gmail.com>
2019-06-22 14:50:38 +0200

    UpdateMap

--- src/ICFP2019/ICFP2019/Algo.cs
+++ src/ICFP2019/ICFP2019/Algo.cs
@@ -6,9 +6,32 @@ namespace ICFP2019
     {
         public void execute(Action a)
         {
-            
+            if (a.IsW) wrappy.Loc += new Point(0, 1);
+            if (a.IsA) wrappy.Loc += new Point(-1, 0);
+            if (a.IsD) wrappy.Loc += new Point(1, 0);
+            if (a.IsS) wrappy.Loc += new Point(0, -1);
+
+            if (a.IsE) wrappy.rotateClockwise();
+            if (a.IsQ) wrappy.rotateAntiClockwise();
+
+            if (a.IsB)
+            {
+                Action.B b = (Action.B) a;
+                wrappy.Manips.Add(new Point(b.Item1, b.Item2));
+                // TODO: checkare che il punto sia davvero adiacente
+            }
+            // TODO: gestire F, L e gli altri booster
+
+            updateMap();
         }
-        
+
+        public void updateMap()
+        {
+            foreach (Point p in wrappy.Manips)
+            {
+//                int x = wrappy.Loc.x + p.x 
+            }
+        }
 
 
     }
