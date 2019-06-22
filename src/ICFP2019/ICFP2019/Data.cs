7cb892417a4b9a11636a4ebad6f8589465b19c97
Alvise Spanò <spano.alvise@gmail.com>
2019-06-22 12:58:36 +0200

    Create Data.cs

---
+++ src/ICFP2019/ICFP2019/Data.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+
+namespace ICFP2019
+{
+
+    enum Tile
+    {
+        Empty = 0, Filled = 1, Obstacle = 2
+    }
+
+    class Map
+    {
+        private Tile[,] a;
+        private int w, h;
+
+        public Map(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+            a = new Tile[w, h];
+            Array.Clear(a, 0, w * h);
+        }
+
+    }
+
+    class Status
+    {
+        private Map map;
+
+        public Status()
+        {
+
+        }
+}
