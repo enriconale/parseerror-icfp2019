4697e9d1ed6329259ef02b39103284b419d55214
ssmaniot <ssmaniot@gmail.com>
2019-06-23 12:27:28 +0200

    Alternative distMap algorithm

--- src/ICFP2019/ICFP2019/Parser.cs
+++ src/ICFP2019/ICFP2019/Parser.cs
@@ -117,6 +117,12 @@ namespace ICFP2019
         public int x;
         public int y;
 
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
         public Point(int x, int y)
         {
             this.x = x;
---
+++ src/ICFP2019/ICFP2019/Properties/ShortestTileAlgo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ICFP2019.Properties
+{
+    public class ShortestTileAlgo
+    {
+        private Map<Tile> map;
+        public static readonly int UNREACHABLE = Int32.MaxValue;
+
+        public ShortestTileAlgo(Map<Tile> map)
+        {
+            this.map = map;
+        }
+
+        private int minAround(Map<int> distMap, Point p)
+        {
+            int min = Int32.MaxValue;
+            if (p.x > 0 && distMap[p.x - 1, p.y] < min) min = distMap[p.x - 1, p.y];
+            if (p.x < map.W && distMap[p.x + 1, p.y] < min) min = distMap[p.x + 1, p.y];
+            if (p.y > 0 && distMap[p.x, p.y - 1] < min) min = distMap[p.x, p.y - 1];
+            if (p.x < map.H && distMap[p.x, p.y + 1] < min) min = distMap[p.x, p.y + 1];
+            return min;
+        }
+
+        private Map<int> CalculateMap(Wrappy w)
+        {
+            Map<int> distMap = new Map<int>(map.W, map.H);
+
+            for (int i = 0; i < map.W; ++i)
+                for (int j = 0; j < map.H; ++j)
+                    distMap.fillWith(UNREACHABLE);
+            distMap[w.Loc] = 0;
+
+            Point p = new Point();
+
+            // cycle through all L-1 norm radii
+            for (int r = 1; r < Math.Max(map.W, map.H); ++r)
+            {
+                // cycle through rhombus (i.e. L-1 circle) side length, coordinates
+                // are computed as (i, r-i), depends on which side
+                for (int i = 0; i < r; ++i)
+                {
+                    // compute offset for each side, sign of x/y offset 
+                    // based on side is:
+                    //
+                    //      1     @     0
+                    //     -/+  @   @  +/+
+                    //        @       @
+                    //      @     X     @
+                    //        @       @
+                    //     -/-  @   @  -/+
+                    //      2     @     3
+                    //
+                    for (int side = 0; side < 4; ++side)
+                    {
+                        int xSign = (side < 2) ? 1 : -1;
+                        int ySign = side % 2;
+                        p.x = w.Loc.x + i * xSign;
+                        p.y = w.Loc.y + (r - i) * ySign;
+                        if (map.validCoordinate(p) && map[p] != Tile.Obstacle)
+                            distMap[p] = minAround(distMap, p) + 1;
+                    }
+                }
+            }
+            return distMap;
+        }
+    }
+}
