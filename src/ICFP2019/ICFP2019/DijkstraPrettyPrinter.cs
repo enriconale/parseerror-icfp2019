4ede1e2da62abfcaf5d63deffb137b9ca84c2afa
Enrico Naletto <enrico.naletto@gmail.com>
2019-06-23 15:35:37 +0200

    add dijkstra pretty printer

--- src/ICFP2019/ICFP2019/DijkstraPrettyPrinter.cs
+++ src/ICFP2019/ICFP2019/DijkstraPrettyPrinter.cs
@@ -6,13 +6,13 @@ namespace ICFP2019
 {
     public class DijkstraPrettyPrinter
     {
-        public static void printDijkstraMap(Map<Tile> parsedMap, Wrappy wrappy, string printerPath)
+        public static void printDijkstraMap(Status status, Wrappy wrappy, string printerPath)
         {
             System.IO.StreamWriter file =
                 new System.IO.StreamWriter(printerPath, false);
             file.WriteLine("================ Distance map for wrappy at position (" + wrappy.Loc.x + ", " + wrappy.Loc.y + ") ================");
             file.WriteLine("================ Wrappy is identified by a ● on the map ================");
-
+            Map<Tile> parsedMap = status.Map;
             for (int i = parsedMap.H - 1; i >= 0; i--)
             {
                 for (int j = 0; j < parsedMap.W; j++)
